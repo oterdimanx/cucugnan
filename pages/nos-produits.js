883745a3a48b58445bcf77b4596f5f3dd5a3c371
oterdiman <olivier.terdiman@gmail.com>
2024-10-08 16:18:26 +0200

    meta robots

--- pages/nos-produits.js
+++ pages/nos-produits.js
@@ -8,7 +8,7 @@ export default function HomeProducts() {
     <div>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="robots" content="noindex,nofollow" />
+        <meta name="robots" content="index,follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="Les 7 saveurs de Cucugnan est un producteur français d'épices aromatisées, d'olives et autres produits du terroir, et un gite d'accueil, dans la région de Perpignan et des Corbières" />
         <meta name="author" content="Les 7 saveurs de Cucugnan est un producteur français d'épices." />
